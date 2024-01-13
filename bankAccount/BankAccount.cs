bc5c5db400d330e94791abe05801ccc81a9c408b
Sam Jacob <sam21nw@gmail.com>
2024-01-13 06:53:54 +0100

    canal lock project added

---
+++ bankAccount/BankAccount.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+public class BankAccount
+{
+  public string Number { get; }
+  public string Owner { get; }
+  public decimal Balance
+  {
+    get
+    {
+      decimal balance = 0;
+      foreach (var item in _allTransactions)
+      {
+        balance += item.Amount;
+      }
+      return balance;
+    }
+  }
+
+  private static int s_accountNumberSeed = 1234567890;
+
+  public BankAccount(string name, decimal initialBalance)
+  {
+    Number = s_accountNumberSeed.ToString();
+    s_accountNumberSeed++;
+
+    this.Owner = name;
+    MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+  }
+  private List<Transaction> _allTransactions = new List<Transaction>();
+
+  public void MakeDeposit(decimal amount, DateTime date, string note)
+  {
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+    }
+    var deposit = new Transaction(amount, date, note);
+    _allTransactions.Add(deposit);
+  }
+  public void MakeWithdrawal(decimal amount, DateTime date, string note)
+  {
+    if (amount <= 0)
+    {
+      throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+    }
+    if (Balance - amount < 0)
+    {
+      throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+    }
+    var withdrawal = new Transaction(-amount, date, note); _allTransactions.Add(withdrawal);
+  }
+}
---
+++ canalLock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using canalLock;
+
+var canalGate = new CanalLock();
+
+Console.WriteLine(canalGate);
+
+Console.WriteLine("=============================================");
+Console.WriteLine("Test invalid commands");
+// Open "wrong" gate (2 tests)
+try
+{
+    canalGate = new CanalLock();
+    canalGate.SetHighGate(open: true);
+}
+catch (InvalidOperationException)
+{
+    Console.WriteLine("Invalid operation: Can't open the high gate.Water is low.");
+}
+Console.WriteLine($"Try to open upper gate: {canalGate}");
