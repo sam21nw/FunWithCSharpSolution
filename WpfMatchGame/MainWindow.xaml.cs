a3f1fb21f01187a655f76d34d4f9a6365363703c
Sam Jacob <sam21nw@gmail.com>
2024-01-15 14:02:07 +0100

    just playing

--- WpfMatchGame/MainWindow.xaml.cs
+++ WpfMatchGame/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace WpfMatchGame
 {
@@ -11,34 +12,85 @@ namespace WpfMatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+        private int tenthOfSecondsElapsed;
+        private int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthOfSecondsElapsed / 10f).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play again?";
+            }
         }
 
         private void SetUpGame()
         {
             List<string> animalEmojis = new List<string>()
             {
-                "🐪", "🐪",
-                "🐒", "🐒",
-                "🐋", "🐋",
-                "🐨", "🐨",
-                "🦙", "🦙",
-                "🐈", "🐈",
-                "🐅", "🐅",
-                "🦮", "🦮",
+                "🐪","🐒","🐋","🐨","🦙","🐈","🐅","🦮","🦖","🦅","🐓","🦆","🐦‍","🦜","🦢","🕊️","🦉"
             };
 
             var random = new Random();
-            foreach (var textBlock in mainGrid.Children.OfType<TextBlock>())
+            //foreach (var textBlock in mainGrid.Children.OfType<TextBlock>())
+            //{
+            //    if (textBlock.Name != "timeTextBlock")
+            //    {
+            //        var index = random.Next(animalEmojis.Count);
+            //        string nextEmoji = animalEmojis[index];
+            //        textBlock.Text = nextEmoji;
+            //        animalEmojis.RemoveAt(index);
+            //    }
+            //}
+
+            timer.Start();
+            tenthOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        private TextBlock lastTextBlockClicked;
+        private bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = (TextBlock)sender;
+            if (findingMatch == false)
             {
-                var index = random.Next(animalEmojis.Count);
-                string nextEmoji = animalEmojis[index];
-                textBlock.Text = nextEmoji;
-                animalEmojis.RemoveAt(index);
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,19 +1,84 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
-        static int Fib(int n)
+        List<string> animalEmojis = new List<string>()
         {
-            if (n < 2)
-            {
-                return n;
-            }
+            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
+        };
 
-            return Fib(n - 1) + Fib(n - 2);
+        var listNums = new List<string>();
+
+        for (int i = 0;i < 12;i++)
+        {
+            var randomIndex = new Random().Next(animalEmojis.Count);
+
+            if (!listNums.Contains(animalEmojis[randomIndex]))
+            {
+                listNums.Add(animalEmojis[randomIndex]);
+            }
         }
 
-        int fib = Fib(10);
+        foreach (var item in listNums)
+        {
+            Console.WriteLine(item);
+        }
 
-        Console.WriteLine(fib);
+        //string[,] myMatrix = new string[4, 4];
+        //for (int i = 0;i < 4;i++)
+        //{
+        //    for (int j = 0;j < 4;j++)
+        //    {
+        //        myMatrix[i, j] = animalEmojis[i];
+        //    }
+        //}
+
+        //for (int i = 0;i < 4;i++)
+        //{
+        //    for (int j = 0;j < 4;j++)
+        //    {
+        //        Console.Write(myMatrix[i, j] + "\t");
+        //    }
+        //    Console.WriteLine();
+        //}
+        //Console.WriteLine();
+
+        //string[][] jaggedArray =
+        //[
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //    ["", ""],
+        //];
+
+        //var random = new Random();
+
+        //int i = 0;
+        //foreach (var subArray in jaggedArray)
+        //{
+        //    var index = random.Next(animalEmojis.Count);
+        //    int j = 0;
+        //    foreach (var element in subArray)
+        //    {
+        //        jaggedArray[i][j] = animalEmojis[index];
+        //        j++;
+        //        animalEmojis.RemoveAt(index);
+        //    }
+        //    i++;
+        //}
+        //foreach (var subArray in jaggedArray)
+        //{
+        //    int j = 0;
+        //    foreach (var element in subArray)
+        //    {
+        //        Console.WriteLine($"Element at jaggedArray[{i}][{j}] is: {element}");
+        //        j++;
+        //    }
+        //    i++;
+        //}
     }
 }
