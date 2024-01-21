33c550cdc804fe6f007c20ae9d8ff95078e627af
Sam Jacob <sam21nw@gmail.com>
2024-01-21 19:44:04 +0100

    fun with generics

--- FunWithGenerics/Program.cs
+++ FunWithGenerics/Program.cs
@@ -1,4 +1,22 @@
 ﻿
-List<T> genericCollection = new();
+using System;
+using System.Collections.Generic;
 
-System.Console.WriteLine(genericCollection);
+int[] myInts = { 3, 6, 8, 11, 14 };
+
+System.Console.WriteLine(Array.BinarySearch<int>(myInts, 10));
+
+Stack<int> stackOfInts = new();
+stackOfInts.Push(5);
+stackOfInts.Push(3);
+stackOfInts.Push(11);
+stackOfInts.Push(2);
+
+System.Console.WriteLine(stackOfInts.Contains(22));
+
+static void Swap<T>(ref T a, ref T b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
