c6c79ddb0fcaa840e5e79acaf14fb29a7521b830
Sam Jacob <sam21nw@gmail.com>
2024-01-26 12:19:28 +0100

    fun with events

--- FunWithDelegatesAndEvents/Program.cs
+++ FunWithDelegatesAndEvents/Program.cs
@@ -2,15 +2,21 @@
 Console.WriteLine("Hello, World!");
 
 MyDelegate d = new(Display);
-BinaryOp<int> bAdd = new(Add);
-BinaryOp<int> bSub = new(Sub);
+//BinaryOp<int> bAdd = new(Add);
+//BinaryOp<int> bSub = new(Sub);
 
-BinaryOp<int> bOp = bAdd;
-bOp += bSub;
+//BinaryOp<int> bOp = bAdd + bSub;
 
 d.Invoke("oops");
 //Console.WriteLine(bOp.Invoke(13, 17));
-bOp.Invoke(24, 67);
+//bOp.Invoke(24, 67);
+
+Console.WriteLine("---------- Func -----------");
+//Func<int, int, int> sum = Add;
+//Console.WriteLine(sum(51, 87));
+
+Action<int, int> sum = Add;
+sum(34, 89);
 
 static void Display(string message)
 {
@@ -25,6 +31,11 @@ static void Sub(int x, int y)
     Console.WriteLine(x - y);
 }
 
-public delegate void BinaryOp<T>(T x, T y);
+static void Dud<T>()
+{
+    throw new NotImplementedException();
+}
+
+public delegate T BinaryOp<T>(T x, T y);
 
 public delegate void MyDelegate(string message);
---
+++ FunWithEvents/Program.cs
@@ -0,0 +1,68 @@
+﻿
+HR hr = new();
+EmployeeCare _ = new(hr);
+hr.RegisterEmployee("dude", "male", 33);
+
+
+class EmployeeCare
+{
+    public EmployeeCare(HR hr)
+    {
+        hr.NewEmployee += Hr_NewEmployee;
+    }
+
+    private void Hr_NewEmployee(object sender, NewEmployeeEventArgs e)
+    {
+        Console.WriteLine("Sender of Event: " + sender);
+        Console.WriteLine($"Customer Info: {e.Name}, {e.Sex}, {e.Age}");
+    }
+}
+
+class HR
+{
+    public event NewEmployeeEventHandler NewEmployee;
+
+    protected void OnNewEmployee(NewEmployeeEventArgs e)
+    {
+        NewEmployee?.Invoke(this, e);
+    }
+
+    public void RegisterEmployee(string name, string sex, int age)
+    {
+        NewEmployeeEventArgs e = new(name, sex, age);
+        OnNewEmployee(e);
+    }
+}
+
+public delegate void NewEmployeeEventHandler(object sender, NewEmployeeEventArgs e);
+
+public class NewEmployeeEventArgs
+{
+    public string Name;
+    public string Sex;
+    public int Age;
+
+    public NewEmployeeEventArgs(string name, string sex, int age)
+    {
+        Name = name;
+        Sex = sex;
+        Age = age;
+    }
+}
+
+//MyDelegate d = new(MyMethod);
+//d += new MyDelegate(MyMethod2);
+
+//d("Hello");
+
+
+//static void MyMethod(string text)
+//{
+//    Console.WriteLine("My method 1: " + text);
+//}
+//static void MyMethod2(string text)
+//{
+//    Console.WriteLine("My method 2: " + text);
+//}
+
+//public delegate void MyDelegate(string text);
