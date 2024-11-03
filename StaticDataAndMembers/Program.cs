c638e145ef9a376d380f3807712675c327818de7
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-03 12:16:28 +0100

    static data and members

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StaticDataAndMembers;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Fun with Static Data *****\n");
+        SavingsAccount s1 = new SavingsAccount(50);
+        SavingsAccount s2 = new SavingsAccount(100);
+
+        Console.WriteLine(s2.GetBalance());
+        Console.WriteLine(SavingsAccount.GetInterestRate());
+
+        SavingsAccount.SetInterestRate(0.08);
+        Console.WriteLine(SavingsAccount.GetInterestRate());
+
+        Console.ReadLine();
+    }
+}
+
+class SavingsAccount
+{
+    private static double s_currInterestRate = 0.04;
+    private double _currBalance;
+    public SavingsAccount(double initialBalance)
+    {
+        _currBalance = initialBalance;
+    }
+    public static void SetInterestRate(double newRate)
+    {
+        s_currInterestRate = newRate;
+    }
+    public static double GetInterestRate()
+    {
+        return s_currInterestRate;
+    }
+    public double GetBalance()
+    {
+        return _currBalance * (1 - s_currInterestRate);
+    }
+}
