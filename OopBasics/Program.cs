3081a18080095f25d98048e24c5a11dddcbccca7
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-03 15:12:21 +0100

    oop basics learn, cars, radio

---
+++ OopBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OopBasics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car viper = new Car();
+        viper.StartCar();
+
+        Console.ReadLine();
+    }
+}
+
+class Radio
+{
+    public void Power(bool turnOn)
+    {
+        Console.WriteLine($"Radio on: {turnOn}");
+    }
+}
+
+class Car
+{
+    private readonly Radio? myRadio;
+    public Car()
+    {
+        myRadio = new Radio();
+    }
+    public void StartCar()
+    {
+        var radio = new Radio();
+        Console.WriteLine($"Car started");
+        radio.Power(true);
+    }
+    public void TurnOnRadio(bool OnOff)
+    {
+        myRadio?.Power(OnOff);
+    }
+}
+
