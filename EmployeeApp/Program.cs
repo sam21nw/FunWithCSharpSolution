7ae88379203d67b6c8192e58b8017c94740a4116
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-04 16:25:15 +0100

    employee app learn encapsualtion, fields, properties

---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeApp;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("***** Fun with Encapsulation *****");
+
+        Employee emp = new Employee("Marvin", 45, 123, 1000, "111-11-1111", EmployeePayTypeEnum.
+Hourly, new DateTime(2023, 12, 2));
+        emp.DisplayStats();
+        emp.GiveBonus(100);
+        emp.DisplayStats();
+
+        Console.ReadLine();
+    }
+}
+
+public enum EmployeePayTypeEnum
+{
+    Hourly,
+    Salaried,
+    Commission
+}
+
---
+++ EmployeeApp/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeApp;
+class SavingsAccount
+{
+    private static double s_currInterestRate = 0.04;
+    private double _currBalance;
+
+    public static double InterestRate
+    {
+        get { return s_currInterestRate; }
+        set { s_currInterestRate = value; }
+    }
+    public SavingsAccount(double initialBalance)
+    {
+        _currBalance = initialBalance;
+    }
+    public static void SetInterestRate(double newRate)
+    {
+        s_currInterestRate = newRate;
+    }
+    public static double GetInterestRate()
+    {
+        return InterestRate;
+    }
+    public double GetBalance()
+    {
+        return _currBalance * (1 + s_currInterestRate);
+    }
+}
