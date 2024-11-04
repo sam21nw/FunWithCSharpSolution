25b558705a3b9cec38a84deef34561e216b2f8fe
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-04 20:41:15 +0100

    basic inheritance learn

---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BasicInheritance;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Basic Inheritance *****\n");
+        // Make a Car object, set max speed and current speed.
+        Car myCar = new Car(80) { Speed = 50 };
+        // Print current speed.
+        Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+        // Now make a MiniVan object.
+        MiniVan myVan = new MiniVan { Speed = 10 };
+        Console.WriteLine("My van is going {0} MPH", myVan.Speed);
+
+        Console.ReadLine();
+    }
+}
+// A simple base class.
+class Car
+{
+    public readonly int MaxSpeed;
+    private int _currSpeed;
+    public Car()
+    {
+        MaxSpeed = 55;
+    }
+    public Car(int max)
+    {
+        MaxSpeed = max;
+    }
+    public int Speed
+    {
+        get { return _currSpeed; }
+        set
+        {
+            _currSpeed = value;
+            if (_currSpeed > MaxSpeed)
+            {
+                _currSpeed = MaxSpeed;
+            }
+        }
+    }
+}
+
+class MiniVan : Car
+{
+}
