ddadb72ebbb248d23328266762750d92b5d99573
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-04 16:45:12 +0100

    auto props, garage, car class created

---
+++ AutoProps/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoProps;
+internal class Car
+{
+    private int numberOfDoors = 2;
+    // Automatic properties!
+    public string? PetName { get; set; }
+    public int Speed { get; set; }
+    public string? Color { get; set; }
+
+    public void DisplayStats()
+    {
+        Console.WriteLine("Car Name: {0}", PetName);
+        Console.WriteLine("Speed: {0}", Speed);
+        Console.WriteLine("Color: {0}", Color);
+    }
+}
---
+++ AutoProps/Garage.cs
@@ -0,0 +1,19 @@
+﻿namespace AutoProps;
+internal class Garage
+{
+    // The hidden int backing field is set to zero!
+    public int NumberOfCars { get; set; } = 1;
+    // The hidden Car backing field is set to null!
+    public Car? MyAuto { get; set; } = new Car();
+
+    // Must use constructor to override default
+    // values assigned to hidden backing fields
+    public Garage()
+    {
+    }
+    public Garage(Car car, int number)
+    {
+        MyAuto = car;
+        NumberOfCars = number;
+    }
+}
