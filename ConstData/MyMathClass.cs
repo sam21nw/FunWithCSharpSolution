e5dc3129d28aa91397739ba865a67d80dfa7dd38
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-08 18:01:34 +0100

    const data

---
+++ ConstData/MyMathClass.cs
@@ -0,0 +1,5 @@
+﻿namespace ConstData;
+internal class MyMathClass
+{
+    public const double PI = 3.14;
+}
---
+++ ConstData/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConstData;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("***** Fun with Const *****");
+
+        Console.ReadLine();
+    }
+}
