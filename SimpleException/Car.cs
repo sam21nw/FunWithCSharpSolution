ade70b0580400a1d34da02ba22c22180240c632f
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-09 06:25:32 +0100

    simple exception

---
+++ SimpleException/Car.cs
@@ -0,0 +1,52 @@
+﻿namespace SimpleException;
+
+class Car
+{
+    public const int MaxSpeed = 100;
+    public string Name { get; set; } = "";
+    public int Speed { get; set; } = 0;
+    private bool _isCarDead = false;
+    private readonly Radio _theMusicBox = new Radio();
+    public Car()
+    {
+
+    }
+    public Car(string name, int speed)
+    {
+        Speed = speed;
+        Name = name;
+    }
+    public void CrankTunes(bool state)
+    {
+        _theMusicBox.TurnOn(state);
+    }
+    public void Accelerate(int delta)
+    {
+        if (_isCarDead)
+        {
+            Console.WriteLine($"{Name} is out of order...");
+        }
+        else
+        {
+            Speed += delta;
+            if (Speed > MaxSpeed)
+            {
+                Speed = 0;
+                _isCarDead = true;
+                throw new Exception($"{Name} has overheated!")
+                {
+                    HelpLink = "http://www.google.com",
+                    Data =
+                    {
+                        {"TimeStamp", $"The car exploded at {DateTime.UtcNow}" },
+                        {"Cause", "You have a lead foot." }
+                    }
+                };
+            }
+            else
+            {
+                Console.WriteLine($"=>  Current speed = {Speed}");
+            }
+        }
+    }
+}
---
+++ SimpleException/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace SimpleException;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("***** Simple Exception Example *****");
+        Console.WriteLine("=> Creating a car and stepping on it!");
+        Car car = new Car("Zippy", 50);
+        car.CrankTunes(true);
+        try
+        {
+            for (int i = 0;i < 10;i++)
+            {
+                car.Accelerate(10);
+            }
+        }
+        catch (Exception e)
+        {
+            // Handle the thrown exception.
+            Console.WriteLine("\n*** Error! ***");
+            Console.WriteLine("Method: {0}", e.TargetSite?.DeclaringType);
+            Console.WriteLine("Method: {0}", e.TargetSite?.MemberType);
+            Console.WriteLine("Message: {0}", e.Message);
+            Console.WriteLine("Source: {0}", e.Source);
+            Console.WriteLine($"Help Link: {e.HelpLink}");
+            foreach (DictionaryEntry de in e.Data)
+            {
+                Console.WriteLine($"{de.Key}: {de.Value}");
+            }
+        }
+        Console.WriteLine("\n***** Out of exception logic *****");
+
+        Console.ReadLine();
+
+    }
+}
