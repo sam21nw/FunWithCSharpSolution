802f759672d5f181c4e6e978be27a5b9f66cc3a5
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-09 09:15:07 +0100

    custom interfaces

---
+++ CustomInterfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomInterfaces;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** A First Look at Interfaces *****\n");
+        CloneableExample();
+        Console.ReadLine();
+    }
+
+    static void CloneableExample()
+    {
+        string myStr = "Hello";
+        OperatingSystem winOS = new(PlatformID.Win32NT, new Version());
+
+        CloneMe(myStr);
+        CloneMe(winOS);
+    }
+    static void CloneMe(ICloneable c)
+    {
+        object theClone = c.Clone();
+        Console.WriteLine($"Your clone is a: {theClone.GetType().Name}");
+    }
+}
+
