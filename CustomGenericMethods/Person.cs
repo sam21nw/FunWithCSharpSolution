879d2b8e4797662ed29581accbffdc4b7b4557f3
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-11 05:01:42 +0100

    custom generic methods

---
+++ CustomGenericMethods/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomGenericMethods;
+
+public class Person
+{
+    public int Age { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public Person() { }
+
+    public Person(string fn, string ln, int age)
+    {
+        FirstName = fn;
+        LastName = ln;
+        Age = age;
+    }
+    public override string ToString()
+    {
+        return $"Name. {FirstName} {LastName}, Age: {Age}";
+    }
+}
---
+++ CustomGenericMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomGenericMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Custom generic functions *****");
+        int a = 3, b = 5;
+        SwapFunctions.Swap(ref a, ref b);
+        Console.WriteLine(String.Format($"{a}, {b}"));
+
+        Console.WriteLine("--------------------------");
+        Person p1 = new("Dude", "Rock", 32);
+        Person p2 = new("Manuel", "Blonde", 36);
+        SwapFunctions.Swap<Person>(ref p1, ref p2);
+        Console.WriteLine(String.Format($"{p1}, {p2}"));
+
+        Console.ReadLine();
+    }
+}
+
+public static class SwapFunctions
+{
+    public static void Swap(ref int a, ref int b)
+    {
+        //(b, a) = (a, b);
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        Console.WriteLine("You sent the Swap() method a {0}", typeof(T));
+        (b, a) = (a, b);
+    }
+}
