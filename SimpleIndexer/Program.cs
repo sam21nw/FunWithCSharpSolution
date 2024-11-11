1b9867e958be53888159f2db412bbeb78822aad2
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-11 18:26:13 +0100

    simple indexer

---
+++ SimpleIndexer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Data;
+namespace SimpleIndexer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Indexers allow you to access items in an array-like fashion.
+        Console.WriteLine("***** Fun with Indexers *****\n");
+    }
+}
+
+public class PersonCollection : IEnumerable
+{
+    private ArrayList arPeople = new ArrayList();
+
+    public Person this[int index]
+    {
+        get => (Person)arPeople[index];
+        set => arPeople.Insert(index, value);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
