3b1e0713f5a5a34904d396c163071cfe6ba9a27c
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-12 17:07:10 +0100

    directory app

---
+++ DirectoryApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace DirectoryApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Fun with Directory(Info) *****\n");
+        DisplayImageFiles();
+        Console.ReadLine();
+    }
+    static void ShowWindowsDirectoryInfo()
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(@".");
+        Console.WriteLine("***** DirectoryInfo *****");
+        Console.WriteLine(directoryInfo.Exists);
+        Console.WriteLine(directoryInfo.FullName);
+        Console.WriteLine(directoryInfo.Name);
+        Console.WriteLine(directoryInfo.LinkTarget);
+        Console.WriteLine(directoryInfo.Attributes);
+    }
+    static void DisplayImageFiles()
+    {
+        DirectoryInfo directory = new DirectoryInfo($@"C{Path.VolumeSeparatorChar}{Path.DirectorySeparatorChar}Windows{Path.DirectorySeparatorChar}Web{Path.DirectorySeparatorChar}Wallpaper");
+        FileInfo[] imageFiles = directory.GetFiles("*.jpg", SearchOption.AllDirectories);
+        Console.WriteLine("Path: " + directory.ToString());
+        foreach (var f in imageFiles)
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine(f.Name);
+            Console.WriteLine(f.Length);
+            Console.WriteLine(f.CreationTime);
+            Console.WriteLine(f.Attributes);
+        }
+    }
+}
