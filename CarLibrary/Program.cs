9e618efac1b262e7f55873cc09713bd559069476
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-12 15:57:51 +0100

    car library updated

--- CarLibrary/Program.cs
+++ CarLibrary/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace CarLibrary;
+﻿using System.Reflection;
+
+namespace CarLibrary;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        MiniVan mv = new MiniVan();
+        //Type t = Type.GetType("CarLibrary.Car", false, true);
+        Type t = typeof(Car);
+        ListMethods(t);
+        Console.WriteLine("----------------");
+        ListFields(t);
+
+        Console.ReadLine();
+
+    }
+    static void ListMethods(Type type)
+    {
+        Console.WriteLine("***** Methods *****");
+        MethodInfo[] methodInfos = type.GetMethods();
+        foreach (MethodInfo mi in methodInfos)
+        {
+            Console.WriteLine(mi.Name);
+            foreach (var p in mi.GetParameters())
+            {
+                Console.WriteLine("-> " + p.ParameterType);
+            }
+        }
+    }
+    static void ListFields(Type type)
+    {
+        Console.WriteLine("***** Fields *****");
+        FieldInfo[] fieldInfos = type.GetFields();
+        foreach (var fi in fieldInfos)
+        {
+            Console.WriteLine(fi.Name);
+        }
     }
 }
