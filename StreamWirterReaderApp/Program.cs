d8e57e4aa8050a59798987ad8fd13ea578349f73
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-13 16:11:45 +0100

    stream reader writer

---
+++ StreamWirterReaderApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***** Fun with StreamWriter / Reader App *****");
+using (StreamWriter writer = File.CreateText("reminders.txt"))
+{
+
+    writer.WriteLine("Don't forget Mother's Day this year...");
+    writer.WriteLine("Don't forget Father's Day this year...");
+    writer.WriteLine("Don't forget these numbers:");
+    for (int i = 0; i < 10; i++)
+    {
+        writer.Write(i + " ");
+    }
+    writer.Write(writer.NewLine);
+}
+Console.WriteLine("Created file and wrote some thoughts...");
+
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("Here are your thoughts:\n");
+using (StreamReader sr = File.OpenText("reminders.txt"))
+{
+    string? input;
+    while ((input = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(input);
+
+    }
+}
+
+Console.ReadLine();
