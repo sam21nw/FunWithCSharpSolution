0b017b74a4eff37a3a4d020ba058fde739a4cd04
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-13 06:31:52 +0100

    file stream app

---
+++ FileStreamApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FileStreamApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Fun with FileStreams *****");
+        using FileStream fileStream = File.Open("myMessage.dat", FileMode.Create);
+        string message = "Hello World!";
+        byte[] msgAsByteArray = Encoding.Default.GetBytes(message);
+
+        fileStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
+        fileStream.Position = 0;
+
+        Console.WriteLine("Your message as an array of bytes: ");
+        byte[] bytesFromFile = new byte[msgAsByteArray.Length];
+        for (int i = 0; i < msgAsByteArray.Length; i++)
+        {
+            bytesFromFile[i] = (byte)fileStream.ReadByte();
+        }
+        Console.WriteLine(String.Join(", ", bytesFromFile));
+
+        Console.Write("\nDecoded Message: ");
+        Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
+        Console.ReadLine();
+        //File.Delete("myMessage.dat");
+    }
+}
