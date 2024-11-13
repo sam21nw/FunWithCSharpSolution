8d73319dc6e16eeaf6b65adfb60f562ba7418ddc
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-13 04:58:41 +0100

    file IO

---
+++ SimpleFileIO/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleFileIO;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Simple IO with the File type *****");
+        var fileName = $@"C{Path.VolumeSeparatorChar}{Path.DirectorySeparatorChar}temp{Path.DirectorySeparatorChar}Test.dat";
+
+        //var f = new FileInfo(fileName);
+        using FileStream fs = File.Open(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+        fs.Close();
+        using (StreamWriter sw = File.AppendText(fileName))
+        {
+            if (sw.NewLine == "")
+            {
+                sw.WriteLine("New Line");
+            }
+        }
+        using (StreamReader sr = File.OpenText(fileName))
+        {
+            string? line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        Console.ReadLine();
+    }
+}
