bdd7ee5d71d5de5c83ea93690c3ba976d264b7eb
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-13 16:26:42 +0100

    string reader writer app

---
+++ StringReaderWriterApp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+Console.WriteLine("***** Fun with StringWriter / StringReader *****\n");
+
+using (StringWriter strWriter = new())
+{
+    strWriter.WriteLine("OOps, I did it again.");
+    Console.WriteLine(strWriter);
+
+    StringBuilder sb = new();
+    sb.Insert(0, "Hey!! ");
+    Console.WriteLine($"-> {sb}");
+    sb.Remove(0, "He".Length);
+    Console.WriteLine("-> " + sb);
+
+    Console.WriteLine("----------------------------------");
+
+    using StringReader sr = new(strWriter.ToString());
+    string? input;
+    while ((input = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(input);
+
+    }
+}
