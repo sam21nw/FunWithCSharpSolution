596b3750d5c5969d237325ef5647a5eed2ff9854
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-11-13 20:55:36 +0100

    simple serialize learn

---
+++ SimpleSerialize/Program.cs
@@ -0,0 +1,42 @@
+﻿global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Xml;
+global using System.Xml.Serialization;
+
+static void SaveAsXmlFormat<T>(T objGraph, string fileName)
+{
+    //Must declare type in the constructor of the XmlSerializer
+    XmlSerializer xmlFormat = new(typeof(T));
+    using Stream fStream = new FileStream(fileName,
+    FileMode.Create, FileAccess.Write, FileShare.None);
+    xmlFormat.Serialize(fStream, objGraph);
+}
+
+Console.WriteLine("***** Fun with Object Serialization *****\n");
+
+Car car = new("dude", 32, true);
+
+SaveAsXmlFormat(car, "CarData.xml");
+Console.WriteLine("=> Saved car in XML format!");
+
+public class Car
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    [XmlAttribute]
+    public Radio radio = new();
+    public Car()
+    {
+
+    }
+    public Car(string name, int age, bool playing)
+    {
+        Name = name;
+        Age = age;
+        radio.Playing = playing;
+    }
+}
+public class Radio
+{
+    public bool Playing { get; set; }
+}
